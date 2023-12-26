4b1ade9d1cd98e18e8a90682690dad8369f9ba09
ocebert31 <oceanebertrand339@gmail.com>
2023-12-26 11:51:41 +0100

    création boutons

--- FrontEnd/js/script.js
+++ FrontEnd/js/script.js
@@ -2,33 +2,74 @@
 
 console.log("hello-world");
 
-// ’appel à l’API avec fetch afin de récupérer dynamiquement les projets de l’architecte
-
-fetch("http://localhost:5678/api/works")
-.then(reponse => reponse.json())
-.then(data => console.log(data))
-
 // supprimer les travaux
-
 // quand le document est chargé, on applique la fonction
 document.addEventListener("DOMContentLoaded", function() {
-    var gallery = document.querySelector(".gallery");
+    let gallery = document.querySelector(".gallery");
     gallery.innerHTML = '';
+
+    // ’appel à l’API avec fetch afin de récupérer dynamiquement les projets de l’architecte
+    fetch("http://localhost:5678/api/works")
+    .then(reponse => reponse.json())
+    .then(data => afficherTravaux(data, gallery)) // fonction asynchrone qui sera executée quand la reponse de fetch arrivera
 });
 
 // ajout des travaux dans la galerie
 
-data.forEach(data => {
-    var figure = document.createElement("figure")
-    var image = document.createElement("img")
-    var figcaption = document.createElement("figcaption")
+function afficherTravaux(data, gallery) {
+    data.forEach(element => { //data = tableau, element = element en cours du tableau
+        let figure = document.createElement("figure")
+        let image = document.createElement("img")
+        let figcaption = document.createElement("figcaption")
+    
+        image.src = element.imageUrl;
+        image.alt = element.title;
+        figcaption.textContent = element.title;
+    
+        figure.appendChild(image);
+        figure.appendChild(figcaption);
+        gallery.appendChild(figure);
+    });
+}
 
-    image = data.imageUrl
-    image = data.title
-    var figcaption.textContent = data.title
 
-    figure.appendChild(image)
-    figure.appendChild(figcaption)
-    gallery.appendChild(figure)
+// 2e appel à l'API
+fetch("http://localhost:5678/api/works")
+.then(resultats => resultats.json())
+.then(réponses => console.table(réponses) )
+
+fetch("http://localhost:5678/api/categories")
+.then(resultat => resultat.json())
+.then(réponse => console.table(réponse) )
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    let gallery = document.querySelector(".gallery");
+
+
+    // Création du bouton "Tous"
+    let boutonTous = document.createElement("button");
+    let contenuTous = document.createTextNode("Tous");
+    boutonTous.appendChild(contenuTous);
+    gallery.appendChild(boutonTous);
+
+    // Création du bouton "Tous"
+    let boutonObjets = document.createElement("button");
+    let contenuObjets = document.createTextNode("Objets");
+    boutonObjets.appendChild(contenuObjets);
+    gallery.appendChild(boutonObjets);
+
+    // Ajout d'un gestionnaire d'événements pour afficher tous les travaux lorsque le bouton "Tous" est cliqué
+    boutonTous.addEventListener("click", function() {
+        showAllProjects();
+    });
+
+    boutonObjets.addEventListener("click", function() {
+        réponse("id=1");
+    });
 });
+
+
+
 
