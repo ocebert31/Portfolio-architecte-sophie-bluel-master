cca7a5a59a233e6688d048c876fe095e51838ace
ocebert31 <153503666+ocebert31@users.noreply.github.com>
2024-01-02 11:47:25 +0100

    page de connexion

--- FrontEnd/js/script.js
+++ FrontEnd/js/script.js
@@ -96,8 +96,11 @@ function linklogin() {
     const footer = document.querySelector("footer");
     footer.style.visibility = "visible";
 
+    const alignement = "display: grid; justify-content: center;";
+
     const formular = document.createElement("form");
-    formular.style = "display: grid; justify-content: center;"
+    formular.setAttribute("onsubmit", "return false");
+    formular.style = alignement;
     login.appendChild(formular);
 
     const legend = document.createElement("legend");
@@ -105,39 +108,70 @@ function linklogin() {
     legend.style = "font-size: 30px; color: #1D6154; font-family: Syne; text-align: center;"
     formular.appendChild(legend);
 
+    const allInputStyle = "display: block; width: 379px; height: 51px; background: rgba(255, 255, 255, 1); box-shadow: 0px 4px 14px 0px rgba(0, 0, 0, 0.09); border-width: 0px;";
+    const dispositionLabel = "display: block; margin-top: 25px; margin-bottom: 10px;"
+
     const labelMail = document.createElement("label");
     labelMail.textContent = "E-mail";
-    labelMail.style = "display: block; margin-top: 25px; margin-bottom: 10px; font-size: 14px;";
+    labelMail.style = dispositionLabel + "font-size: 14px;";
     formular.appendChild(labelMail);
 
     const inputMail = document.createElement("input");
     inputMail.type = "text";
-    inputMail.name = "E-mail";
-    inputMail.style = "display: block; width: 379px; height: 51px; background: rgba(255, 255, 255, 1); box-shadow: 0px 4px 14px 0px rgba(0, 0, 0, 0.09); border-width: 0px;"
+    inputMail.name = "email";
+    inputMail.style = allInputStyle
     formular.appendChild(inputMail);
     
     const labelPassword = document.createElement("label");
     labelPassword.textContent = "Mot de passe";
-    labelPassword.style = "display: block; margin-top: 15px; margin-bottom: 10px;"
+    labelPassword.style = dispositionLabel;
     formular.appendChild(labelPassword);
 
     const inputPassword = document.createElement("input");
     inputPassword.type = "password";
-    inputPassword.name = "mot de passe";
-    inputPassword.style = "display: block; width: 379px; height: 51px; background: rgba(255, 255, 255, 1); box-shadow: 0px 4px 14px 0px rgba(0, 0, 0, 0.09); border-width: 0px";
+    inputPassword.name = "password";
+    inputPassword.style = allInputStyle;
     formular.appendChild(inputPassword); 
 
     const boutonSeConnecter = document.createElement("input");
     boutonSeConnecter.value = "Se connecter";
     boutonSeConnecter.type = "submit";
-    boutonSeConnecter.style = "display: grid; justify-content: center; height: 36px;"
-    login.appendChild(boutonSeConnecter);
+    boutonSeConnecter.style = alignement + "height: 36px;"
+    formular.appendChild(boutonSeConnecter);
 
     const passwordforget = document.createElement("p");
     passwordforget.innerText = "Mot de passe oublié"
-    passwordforget.style = "display: grid; justify-content: center; font-size: 14px; text-decoration-line: underline;"
+    passwordforget.style = alignement + "font-size: 14px; text-decoration-line: underline;"
     login.appendChild(passwordforget);
-});
+
+    formular.addEventListener("submit", async function (event) {
+      console.log("POUET2")
+      event.preventDefault();
+      //creation de l'objet password
+      const passwordLogin = {
+      email: event.target.querySelector("input[name=email]").value,
+      password: event.target.querySelector("input[name=password]").value,
+      };
+      console.log(passwordLogin);
+
+      const combinaisonMailPass = JSON.stringify(passwordLogin);
+
+      const response = await fetch("http://localhost:5678/api/users/login", {
+        method: "POST",
+        headers: {
+          "accept": " application/json",
+          "Content-Type": "application/json" 
+        },
+        body: combinaisonMailPass,
+      });
+      
+      if (response.status === 200) {
+        console.log(response);
+      } else {
+        console.log("la requête n'a pas abouti"); // “Erreur dans l’identifiant ou le mot de passe
+      }
+    });
+  });
 } 
 
 
